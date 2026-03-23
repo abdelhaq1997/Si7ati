f6f4d5ca017478d15a13582c7d304881302a141e
abdelhaq1997 <abdelhaq.jait@gmail.com>
2026-03-23 23:41:10 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,39 @@
-const CACHE_NAME = "sehati-v2";
-const APP_SHELL = ["./", "./index.html", "./manifest.json"];
+const CACHE_NAME = 'sehati-plus-v2-2026-03-23';
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
 
-  const isNavigation = event.request.mode === "navigate";
-
-  event.respondWith((async () => {
-    try {
-      const networkResponse = await fetch(event.request);
-      const cache = await caches.open(CACHE_NAME);
-      if (event.request.url.startsWith(self.location.origin)) {
-        cache.put(event.request, networkResponse.clone());
-      }
-      return networkResponse;
-    } catch (error) {
-      const cached = await caches.match(event.request);
+  event.respondWith(
+    caches.match(req).then(cached => {
       if (cached) return cached;
-      if (isNavigation) return caches.match("./index.html");
-      throw error;
-    }
-  })());
+      return fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
 });
